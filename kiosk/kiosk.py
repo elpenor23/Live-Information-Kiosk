9fd2c53cfe8204d3eac37ad61e3e7935e733a33d
Eric Malinowski <eric.malinowski@gmail.com>
2020-10-31 17:09:16 -0400

    removed uneeded stuff

--- kiosk/kiosk.py
+++ kiosk/kiosk.py
@@ -1,26 +1,8 @@
 #!/usr/bin/python3
 """ base application start """
-import sys, os
-import logging
-import logging.handlers
+import sys
 from PyQt5.QtWidgets import QApplication
 from ui_setup import AppUI
-
-
-# def get_logger():
-#     """ Initializes the logger for the entire program """
-#     # Set up a specific logger with our desired output level
-#     kiosk_logger = logging.getLogger('kiosk_log')
-#     kiosk_logger.setLevel(logging.INFO)
-
-#     # Add the log message handler to the logger
-#     handler = logging.handlers.RotatingFileHandler(
-#                 LOG_FILENAME, maxBytes=2000000, backupCount=5)
-#     log_formatter = logging.Formatter('{asctime}: {levelname:8s} - {message}', style='{')
-#     handler.setFormatter(log_formatter)
-
-#     kiosk_logger.addHandler(handler)
-#     return kiosk_logger
 
 if __name__ == '__main__':
     APP = QApplication(sys.argv)
