813b8d6d795392fb6fb824ceb869b61befb37ef2
Eric Malinowski <eric.malinowski@gmail.com>
2024-01-15 10:52:41 -0500

    Fixed solar bugs

--- kiosk/controllers/solar_data_controller.py
+++ kiosk/controllers/solar_data_controller.py
@@ -6,9 +6,17 @@ class SolarDataController():
         api_config = open_config_file(API_CONFIG_FILE_NAME)
 
         solar_data = get_api_data(api_config["local_solar_endpoint"], {})
+        if "error" not in solar_data:
+            processed_solar_data = SolarDataController.process_solar_data(solar_data)
+        else:
+            processed_solar_data = solar_data
 
-        processed_solar_data = SolarDataController.process_solar_data(solar_data)
-        return solar_data
+        return processed_solar_data
     
     def process_solar_data(solar_data):
-        solar_data["percet_power_generated"] = round((solar_data["currentPower"]/solar_data["maxPower"]) * 100)
+        if solar_data["maxPower"] > 0:
+            solar_data["percet_power_generated"] = round((solar_data["currentPower"]/solar_data["maxPower"]) * 100)
+        else:
+            solar_data["percet_power_generated"] = 0
+
+        return solar_data
